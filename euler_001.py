88fbfe0793f985dadb0356ffbcbaba18471d6490
Justin Zhu <justinthezhu@gmail.com>
2020-04-22 11:22:58 -0700

    Saved new changes 2020-04-22 11:22:58

--- euler_001.py
+++ euler_001.py
@@ -8,5 +8,22 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 
 """
 
+# basic
 multiples = [i for i in range(1000) if i % 3 == 0 or i % 5 == 0]
 print(sum(multiples))
+
+# the math way
+# n = 1000
+# m1 = 3
+# m2 = 5
+
+# def multsum(n, m1):
+#     n //= m1
+#     return int(m1 * n * (n + 1) / 2)
+
+
+# ans = multsum(n, m1) + multsum(n, m2) - multsum(n, m1 * m2)
+# print(ans)
+
+
+
--- euler_002.py
+++ euler_002.py
@@ -10,3 +10,33 @@ Find the sum of all the even-valued terms in the sequence which do not
 exceed four million.
 
 """
+
+# Solution 1
+import math
+
+n1 = 1
+n2 = 1
+sum = 0
+count = 2
+
+while (n2 <= 4e6):
+    n = n1 + n2
+    sum += n if n % 2 == 0 else 0
+    n1, n2 = n2, n
+    count += 1
+
+# print(sum)
+
+# Solution 2
+phi = (1 + math.sqrt(5)) / 2
+psi = (1 - math.sqrt(5)) / 2
+k = count -2
+phi3 = phi * 3
+psi3 = psi * 3
+ans = int((1 / math.sqrt(5)) * (
+        phi3 * ((1 - phi3 ** k) / (1 - phi3)) -
+        psi3 * ((1 - psi3 ** k) / (1 - psi3))
+    ))
+
+print(ans)
+
