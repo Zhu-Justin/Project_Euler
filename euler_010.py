a1eabc96a8e00ab443d2cad1a66b84888107a1f6
Justin Zhu <justinthezhu@gmail.com>
2020-05-17 11:06:44 -0700

    Saved new changes 2020-05-17 11:06:43

--- euler_010.py
+++ euler_010.py
@@ -8,8 +8,22 @@ Find the sum of all the primes below two million.
 """
 import math
 
+# def sum_primes(limit):
+#     primes = []
+#     for n in range(2, limit+1):
+#         # try dividing n with all primes less than sqrt(n):
+#         for p in primes:
+#             if n % p == 0: break     # if p divides n, stop the search
+#             if n < p*p:
+#                primes.append(n)      # if p > sqrt(n), mark n as prime and stop search
+#                break
+#         else: primes.append(n)       # fallback: we actually only get here for n == 2
+#     return sum(primes)
+
+# print(sum_primes(N))
+# fast method
 def primes_sieve(n):
-    p_n = n
+    p_n = n + 1
     # p_n = int(2 * n * math.log(n))       # over-estimate p_n
     sieve = [True] * p_n                 # everything is prime to start
     count = 0
@@ -18,12 +32,12 @@ def primes_sieve(n):
     for i in range(2, p_n):
         if sieve[i]:                       # still prime?
             count += 1                     # count it!
-            ans += sieve[i]
-            if count == n:                 # done!
-                return i
+            ans += i
+            # if count == n:                 # done!
+            #     return i
             for j in range(2*i, p_n, i):   # cross off all multiples of i
                 sieve[j] = False
     return ans
 
-print(primes_sieve(int(2e6)))
-
+N=int(2e6)
+print(primes_sieve(N))
